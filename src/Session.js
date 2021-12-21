b0d35ba4edae3cbdf87e6a9a8b53c17f1785cdb9
m05e5 <calebmoses1111@gmail.com>
2021-12-21 20:35:39 +0100

    refactor

--- src/Session.js
+++ src/Session.js
@@ -1,32 +1,30 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const isConnect = async() => {
-    let answer = false;
-    let token ="";
-    if (localStorage.getItem("token") != null)
-    {
-        token =localStorage.getItem("token")
-    }
-    await axios.get('http://carhubackend.herokuapp.com/member-data', {
-        headers: {
-          authorization: token
-        }
-      })
-    .then(function (response) {
-      return response
-    })
-    .then(response =>{
-      if(response.data.message === "If you see this, you're in!"){
-          answer =true;
+// eslint-disable-next-line import/prefer-default-export
+export const isConnect = async () => {
+  let answer = false;
+  let token = '';
+  if (localStorage.getItem('token') != null) {
+    token = localStorage.getItem('token');
+  }
+  await axios.get('http://carhubackend.herokuapp.com/member-data', {
+    headers: {
+      authorization: token,
+    },
+  })
+    .then((response) => response)
+    .then((response) => {
+      if (response.data.message === "If you see this, you're in!") {
+        answer = true;
       } else {
-        localStorage.setItem("token","");
+        localStorage.setItem('token', '');
       }
-      return answer
+      return answer;
     })
-    .catch(function (error) {
-      console.log(error)
-      alert("there is not internet connection");
-      return error
+    .catch((error) => {
+      console.log(error);
+      alert('there is not internet connection');
+      return error;
     });
-          return answer
-}
+  return answer;
+};
