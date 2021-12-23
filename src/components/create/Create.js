0e75bd794820ef173dde1bbbe6c40d9f6edd8736
eroiyo <eroiyo@outlook.es>
2021-12-23 02:15:41 -0400

    added some inputs

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -1,32 +1,92 @@
-import React, { useEffect } from 'react';
-import { goBack } from '../../Session';
+/*eslint-disable*/
+import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom';
 
-const Create = () => {
-  const navToggle = () => {
-    const homeLink = document.querySelector('#home_link');
-    const createLink = document.querySelector('#create_link');
-    const reserveLink = document.querySelector('#reserve_link');
-    const logoutLink = document.querySelector('#logout_link');
-    const deleteLink = document.querySelector('#delete_link');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './create.css'
+import axios from 'axios';
+class SignupForm extends Component {
+  state =
+  { car: {
+    name: "",
+    description: "",
+    price: 1,
+    fee: 1,
+    horse_power: 1,
+    background_color: "",
+    image: null
+  }
+  }
+  componentDidMount() {
+  }
 
-    logoutLink.classList.remove('selected_nav');
-    createLink.classList.add('selected_nav');
-    homeLink.classList.remove('selected_nav');
-    reserveLink.classList.remove('selected_nav');
-    deleteLink.classList.remove('selected_nav');
-  };
+  newcar = async () => {
+    await axios.post('http://carhubackend.herokuapp.com/models/', this.state)
+      .then((response) => response)
+      .then((response) => {
+        console.log(response.data.message)
+        if(response.data.message == 'Car saved!'){
+          return true
+        } else {
+          alert("All the data should be filed")
+          return false
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
 
-  useEffect(() => {
-    goBack();
-    navToggle();
-  }, []);
+  handleChange=async (e) => {
+    await this.setState({
+      car: {
+        ...this.state.car,
+        [e.target.name]: e.target.value,
+      },
+    });
+    console.log(this.state.car)
+  }
 
-  return (
+  render() {
+    return (
 
-    <div className="container_">
-      <p>Create</p>
-    </div>
-  );
-};
+      <div className="container_">
+        <div className= "add-box">
+        <div className = "login-title">
+        <h1>Add a car</h1>
+        <div className="login-content">
+          <div className="double">
+            <input type="text" className="input" placeholder='name' name="name" onChange={this.handleChange} />
+            <div className= "labeled">
+            <label for="color" className='label' >Color</label>
+            <input type="color" placeholder='#ffffff' name="color" id="color" onChange={this.handleChange}/>
+            </div>
+            </div>
+            <div className="double">
+            <div className= "labeled">
+            <label for="price" className='label' className='label'>Price</label>
+            <input type="number"  className="input linput" placeholder='#ffffff' id="price" name="color" onChange={this.handleChange}/>
+            </div>
+            <div className= "labeled">
+            <p className='label'>Fee</p>
+            <input type="label"  className="input linput" placeholder='#ffffff' name="color" onChange={this.handleChange}/>
+            </div>
+            </div>
+            <div className="double">
+            <div className= "labeled">
+            <p className='label'>Horse Power</p>
+            <input type="label"  className="input linput" placeholder='#ffffff' name="color" onChange={this.handleChange}/>
+            </div>
+            </div>
+            <button className="log-button add-b" onClick={() => this.register()}>Register</button>
+            </div>
+            </div>
+            </div>
+            </div>
+    );
+  }
+}
+export default SignupForm
 
-export default Create;
