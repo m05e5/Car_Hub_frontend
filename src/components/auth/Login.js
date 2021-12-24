4f27a6a4d39493455f9352959039b4d2805e709f
m05e5 <calebmoses1111@gmail.com>
2021-12-24 09:08:03 +0100

    apply changes suggested by reviewer

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,4 @@
-/*eslint-disable*/
+/* eslint-disable */
 import React, { Component } from 'react';
 import {
   Link,
@@ -7,7 +7,7 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './login.css';
 import axios from 'axios';
-import './login.css'
+
 class Login extends Component {
   state = {
     user: {
@@ -17,24 +17,19 @@ class Login extends Component {
   }
 
   componentDidMount() {
+    const mobileNav = document.querySelector('.mobile_nav');
 
-    const mobile_nav = document.querySelector('.mobile_nav');
-
-    mobile_nav.classList.add('invisible');
+    mobileNav.classList.add('invisible');
   }
-
 
   connect = async () => {
     await axios.post('http://carhubackend.herokuapp.com/users/sign_in', this.state)
       .then((response) => response)
       .then((response) => {
-        localStorage.setItem("token", response.headers["authorization"]);
-        window.location.href = "/"
+        localStorage.setItem('token', response.headers.authorization);
+        window.location.href = '/';
       })
-      .catch((error) => {
-        alert('the name or password are incorrects');
-        return error;
-      });
+      .catch((error) => error);
   }
 
   handleChange = async (e) => {
@@ -47,7 +42,6 @@ class Login extends Component {
   }
 
   render() {
-
     return (
 
       <div className="container_login">
@@ -56,14 +50,14 @@ class Login extends Component {
             <h1>Login</h1>
             <div className="login-content">
               <div className="input-field">
-                <input type="email" className="form-control" placeholder='email' name="email" onChange={this.handleChange} />
+                <input type="email" className="form-control" placeholder="email" name="email" onChange={this.handleChange} />
               </div>
               <div className="input-field">
-                <input type="password" className="form-control" placeholder='password' name="password" onChange={this.handleChange} />
+                <input type="password" className="form-control" placeholder="password" name="password" onChange={this.handleChange} />
               </div>
-              <div class="action">
+              <div className="action">
                 <Link className="to-register-button log-b" to="/Signup">Go to Register</Link>
-                <button className="log-button log-b" onClick={() => this.connect()}>Sign in</button>
+                <button type="button" className="log-button log-b" onClick={() => this.connect()}>Sign in</button>
               </div>
             </div>
           </div>
@@ -72,4 +66,4 @@ class Login extends Component {
     );
   }
 }
-export default Login
+export default Login;
